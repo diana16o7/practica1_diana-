0cfa71fcd88ea6341104eb69755bcbd60e521500
diana16o7 <89814125+diana16o7@users.noreply.github.com>
2021-09-06 17:33:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+function getIMC(){
+  var peso=parsefloat(document.getElementById('peso´).value);
+  var altura=parsefloat(document.getElementById('altura').value);
+var imc=peso/(altura*altura);
+  documentgetElementById("imc")innerHTML=imc;
